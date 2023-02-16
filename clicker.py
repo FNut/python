fd70e83395e18001a5808c03e79b12a52812cb01
Artyom Turov <foinution@gmail.com>
2023-02-16 21:41:06 +0300

    Add files via upload

---
+++ clicker.py
@@ -0,0 +1,6 @@
+score = 0
+print(score)
+while True:
+    click = input('')
+    score = score + 1
+    print(score)
---
+++ timer.py
@@ -0,0 +1,8 @@
+import time
+sec = 0
+print(sec)
+click = input('')
+while True:
+    time.sleep(1)
+    sec = sec + 1
+    print(sec)
